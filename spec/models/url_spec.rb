7f885e9ce521b185190a060ee1c6105b4b495821
mayrez <mayrezbritto@gmail.com>
2016-05-15 15:44:37 +0200

    tests

---
+++ spec/models/url_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Url, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+  #url = Url.create('http://www.elpais.es')
+  #assert_equal(url.original, 'http://www.elpais.es')
+  #assert_equal(url.link, 'elpais')
+end
